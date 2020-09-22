71952fb2489c27a30ab6243e523916bd4acdca76
ayman <aymanimtyaz@gmail.com>
2020-09-23 00:19:56 +0530

    Added some documentation to the reset_db.sql file

--- sql_scripts/reset_db.sql
+++ sql_scripts/reset_db.sql
@@ -1,14 +1,20 @@
+/* This script initiallizes the following tables/relations:*/
+
+--transactions: This table records all the individual spending that a user commits to the application.
+--categories: This table holds all the different categories of transactions that the user has defined.
+
+
 CREATE TABLE IF NOT EXISTS transactions(
-	id SERIAL NOT NULL PRIMARY KEY UNIQUE,
+	id SERIAL NOT NULL PRIMARY KEY UNIQUE,                --primary key
 	item TEXT NOT NULL,
 	price INTEGER CHECK(price > 0),
 	vendor TEXT,
-	category_id INTEGER NOT NULL,
+	category_id INTEGER NOT NULL,                         --foreign key, points to the id in the categories table
 	date_of_purchase DATE DEFAULT CURRENT_DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS categories(
-	id SERIAL NOT NULL PRIMARY KEY UNIQUE,
+	id SERIAL NOT NULL PRIMARY KEY UNIQUE,                --primary key
 	category TEXT NOT NULL UNIQUE,
 	descr TEXT
 )
