30c8483cd35397aa61e9209857fc4b040fd99527
ayman <aymanimtyaz@gmail.com>
2020-09-24 19:38:46 +0530

    Added functions that allow basic sending and receiving of messages from a calling function.
    Added functions:
    1. sendMsg()
    2. getMsg()

--- telegrambot.py
+++ telegrambot.py
@@ -6,20 +6,21 @@ and to gain a better understanding of how this wrapper works.
 
 **********************************************************************************************'''
 
-import requests
-import json
-import os
+import requests, json, os
 
 class communicate:
 
     service_url_prefix = 'https://api.telegram.org/bot'
 
-    def __init__(self, offset=0, limit=100, timeout=0, allowed_updates=[]):
+    def __init__(self, offset=0, limit=1, timeout=100, allowed_updates=[], init=0):
         self.token = open(os.getcwd()+'//bot_token.txt').read()
         self.offset = offset
         self.limit = limit
         self.timeout = timeout
         self.allowed_updates = allowed_updates
+        self.data = None
+        self.init = init
+
 
 
     ''' receive() implements the /getUpdates method of the API, all the parameters for this call 
@@ -27,22 +28,48 @@ class communicate:
     def receive(self):
         api_params = {"offset":self.offset, "limit":self.limit,
                       "timeout":self.timeout, "allowed_updates":self.allowed_updates}
+        #print("waiting for input")
         req_obj = requests.get(self.service_url_prefix+self.token+'/getUpdates', 
                                params=api_params)
-        json_resp = req_obj.json()
-        print(json.dumps(json_resp, indent = 4))
+        self.data = req_obj.json()
+        #print(json.dumps(self.data, indent=4))
+        if len(self.data['result'])==0:
+            self.data=None
+            return self.data
+        if self.init == 0:
+            self.offset = self.data['result'][-1]['update_id']
+            self.init+=1
+        self.offset+=1
+        return 
 
 
-    ''' send() implements the /sendMessage method of the API, the necessary parameters for this
+    ''' sendMsg() implements the /sendMessage method of the API, the necessary parameters for this
         call are the chat id which is unique for each chat/user and the text message to send '''
-    def send(self, chat_id, text):
+    def sendMsg(self, chat_id, text):
         api_params = {"chat_id":chat_id, "text":text}
 
         req_obj = requests.get(self.service_url_prefix+self.token+'/sendMessage',
                                params=api_params)
 
         json_resp = req_obj.json()
-        print(json.dumps(json_resp, indent=4))
+        #print(json.dumps(json_resp, indent=4))
+
+
+    ''' getMsg() calls receive() to get a json object back, from which it extracts the oldest
+        untracked message from the API '''
+    def getMsg(self):
+        self.receive()
+        if self.data == None:
+            return self.getMsg()
+        else:
+            message_body=self.data['result'][-1]['message']['text']
+            chat_id=self.data['result'][-1]['message']['chat']['id']
+            update_id=self.data['result'][-1]['update_id']
+            return [message_body, chat_id, update_id]
+
+
+
+
 
 
     
