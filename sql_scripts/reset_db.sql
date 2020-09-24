04d553e6e085fa30f834f06739969fc89acf214e
ayman <aymanimtyaz@gmail.com>
2020-09-24 20:05:42 +0530

    Added code to initiallize table users, in reset_db.sql, along with some documentation

--- sql_scripts/reset_db.sql
+++ sql_scripts/reset_db.sql
@@ -2,7 +2,7 @@
 
 --transactions: This table records all the individual spending that a user commits to the application.
 --categories: This table holds all the different categories of transactions that the user has defined.
-
+--users: This table holds the telegram usernames of all the people who are subscribed to the application.
 
 CREATE TABLE IF NOT EXISTS transactions(
 	id SERIAL NOT NULL PRIMARY KEY UNIQUE,                --primary key
@@ -17,4 +17,9 @@ CREATE TABLE IF NOT EXISTS categories(
 	id SERIAL NOT NULL PRIMARY KEY UNIQUE,                --primary key
 	category TEXT NOT NULL UNIQUE,
 	descr TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users(   
+	id BIGINT NOT NULL PRIMARY KEY UNIQUE,                --primary key, unique, numeric id give by telegram to each user
+	username TEXT UNIQUE                                  --unique telegram username of the user
 )
