e1e9c5c82896a58fa358a71b29bb1fbdb49c874e
ayman <aymanimtyaz@gmail.com>
2020-10-19 01:01:00 +0530

    Created a new file db_interface.py that handles the interaction of the app with the PostgreSQL database using psycopg2.
    The file contains a class pool_init() and two standalone functions
    cnnct() and dscnnct().
    
    The pool_init class initializes a psycopg2 connection pool object
    that can be used by the application to get connections from when
    needed using the cnnct() function, and the connections can be put
    back to standy by using the dscnnct() function. The dscnnct() func-
    -tion also handles commits to the database as well as creation and
    closing of cursors.
    
    This addtion significantly increased the performance of the app in
    terms of the quickness of the bot's replies.
    
    The changes made to other parts of the application that also facil
    -itate the above stated changes will be added in the next commit.

---
+++ db_interface.py
@@ -0,0 +1,29 @@
+import psycopg2 as pg2
+from psycopg2 import pool as pl
+
+class pool_init():
+    
+    data = 'spndr'
+    username = 'postgres'
+    password = 'password'
+    min_con = '1'
+    max_con = '1'
+
+    def __init__(self, function):
+        self.connection_pool = pl.SimpleConnectionPool(self.min_con, self.max_con, database = self.data, 
+                                                             user = self.username, password = self.password)
+        self.function = function
+
+    def __call__(self, *args, **kwargs):
+        return self.function(self.connection_pool, args, kwargs)
+
+@pool_init
+def cnnct(*args):
+    con_obj = args[0].getconn()
+    cur_obj = con_obj.cursor()
+    return args[0], con_obj, cur_obj
+
+def dscnnct(pool, con, curs):
+    con.commit()
+    curs.close()
+    pool.putconn(con)
