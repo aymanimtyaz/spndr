c08fa47d68d7209e7bed27014bf3859e79a2e0e2
ayman <aymanimtyaz@gmail.com>
2020-10-30 12:14:57 +0530

    Added a function init_app() that loads any necessary files that may be needed later on. Currently, file from the replies and sql_scripts direc- tories are loaded.

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import api_caller as ac 
 import message_processor as mp
+from sql_scripts_loader import sql_scripts
+from replies_loader import replies_dicts
 
-while True:
-    sid, msg, cid, uid = ac.getMsg()
-    print('MESSAGE SENT:', msg)
-    message_to_return = mp.return_message(msg, sid, cid)
-    ac.sendMsg(cid, message_to_return)
+def init_app():
+    sql_scripts.scr_dict
+
+    replies_dicts.command_replies
+    replies_dicts.special_replies
+    replies_dicts.standard_transaction
+    replies_dicts.unregistered_senders
+    replies_dicts.wrong_input_replies
+
+def main():
+    while True:
+        sid, msg, cid, uid = ac.getMsg()
+        print('MESSAGE SENT:', msg)
+        message_to_return = mp.return_message(msg, sid, cid)
+        ac.sendMsg(cid, message_to_return)
+
+if __name__ == '__main__':
+    init_app()
+    main()
     
 
     
